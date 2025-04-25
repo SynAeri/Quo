2af39ff7b2735d5a5159e3347f1194c2d5eeed1f
Jordan <164128582+SynAeri@users.noreply.github.com>
2025-04-26 00:22:45 +1000

    Finalised and fixed everything prepared for sharing

--- Oberon/Interface/backend/main.py
+++ Oberon/Interface/backend/main.py
@@ -10,7 +10,7 @@ app = FastAPI()
 # Use cors to connect frontend thing to backend thing
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],  # Frontend URL
+    allow_origins=["*"],  # Frontend URL
     allow_credentials=True,
     allow_methods=["*"],  # Allow all methods
     allow_headers=["*"],  # Allow all headers
