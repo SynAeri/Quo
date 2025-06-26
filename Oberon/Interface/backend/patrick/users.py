78a54a5baba26991bb57a4fef5392176dc5ef2af
patrick chen <maybelonelyz@gmail.com>
2025-06-26 13:48:00 +1000

    Created function: account_analysis. This functions sole purpose is to analyse the various bank accounts of users and provide a verdict of investment. This is the first framework of this function and will be improved on later by collaborative efforts.

--- Oberon/Interface/backend/patrick/users.py
+++ Oberon/Interface/backend/patrick/users.py
@@ -1,5 +1,6 @@
 from collections import defaultdict
 from basiq_manager import BasiqAPI
+from typing import List, Dict, Optional
 
 from transactions import Transaction, AllTransactions
 
@@ -11,10 +12,10 @@ class User:
         self.transactions = self.fetch_transactions()
         self.accounts = self.get_accounts()
     
-    def create_API_Interface(self):
+    def create_API_Interface(self) -> BasiqAPI:
         return BasiqAPI(api_key="OGZmYTY5YWYtODhlMy00YTU3LThmMzMtYTVlMGE3YzA5OGY3Ojk3YzJhODE4LWU5ZjMtNDI5MC1iNzkyLWJkZjI5ZmU5M2NhMg==", User=self)
     
-    def fetch_transactions(self):
+    def fetch_transactions(self) -> List[Transaction]:
         data = self.BasiqManager.getTransactionData(self.filter_transfer)
         if not data:
             return "Error Retrieving Transactions"
@@ -26,11 +27,86 @@ class User:
         
         return transactions
         
-    def get_accounts(self):
+    def get_accounts(self) -> dict:
         return self.BasiqManager.get_accounts()
         
-    def get_transaction_group(self):
+    def get_transaction_group(self) -> AllTransactions:
         return AllTransactions(self.transactions)
+    
+    def account_analysis(self) -> dict:
+        # Here we calculate the sum of balances for a users different accounts
+        # This function is used to calculate positive and negative balanace and aggregate them together
+        
+        def sum_balances(positive: bool = True) -> float:
+            total = 0.0
+            for account in self.accounts:
+                bal = float(account['balance'])
+                if positive and bal > 0:
+                    total += bal
+                elif not positive and bal < 0:
+                    total += bal
+        
+            return total
+        
+        # This function purely calculates how much liquid cash a user has all together.
+        # Right now, im only looking for cheque and savings accounts but we can add more in the future
+        def calculate_liquidation() -> float:
+            keywords = ('cheque', 'savings')
+            liquid_total = 0.0
+            
+            for account in self.accounts:
+                name = account['accountName'].lower()
+                bal = float(account['balance'])
+                
+                if bal > 0:
+                    for keyword in keywords:
+                        if keyword in name:
+                            liquid_total += bal
+                            break
+                        
+            return liquid_total
+        
+        def pick_risk_allocation(surplus_ratio: float) -> str:
+            if surplus_ratio < 0.20: # We can change these ratios for what we can deem fit (Im not really a investor so i don't really know)
+                return "Probably shy away from investing" # We can recommend to our users what they can invest in here, i just added placeholders here
+            if surplus_ratio < 0.50:
+                return "Moderate aggressive in investing"
+            
+            return "Can take risks and be aggressive in investing"
+        
+        liquid_cash = calculate_liquidation()
+        total_negatives = sum_balances(positive=False)
+        total_debt = -total_negatives
+        
+        total_assets = sum_balances(positive=True)
+                
+        investable = liquid_cash
+        
+        if total_assets > 0:
+            # A surplus ratio shows the fraction of your total assets that you are able to invest into
+            
+            surplus_ratio = investable / total_assets ## Investable Cash / Total Positive Assets
+            
+            # A Low Surplus Ratio indicates that you have little liquid cash available (< 0.2) --> We can change this at the pick_risk_allocation function
+            # Medium Surplus Radio indicates that you can take moderate risk in investing (0.2 - 0.5)
+            # High ratio indicates that more than half of you assets are liquid (> 0.5) Hence you can be more aggressive in investing
+            
+        else:
+            surplus_ratio = 0.0
+            
+        verdict = pick_risk_allocation(surplus_ratio)
+        
+        result = {
+        'total_assets':     round(total_assets, 2),
+        'total_debt':       round(total_debt, 2),
+        'investable':       round(investable, 2),
+        'surplus_ratio':    round(surplus_ratio, 2),
+        'verdict':       verdict,
+        }
+        
+        return result
+                
+                
     
 class UserManager:
     def __init__(self):
